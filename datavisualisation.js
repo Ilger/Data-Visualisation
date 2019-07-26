12f0d3dc24926c541c905d9078d76ba4659d66d9
Ilger <ilgerdepraetere@gmail.com>
2019-07-26 12:41:04 +0200

    add in chart script

--- datavisualisation.js
+++ datavisualisation.js
@@ -9,9 +9,66 @@ const table2 = document.getElementById('table2')
 console.log(table1);
 console.log(table2);
 
+// parse table
 var table = document.querySelector("table");
 var data  = parseTable(table);
 console.log(data);
+
+var tbl = $('table#students tr').map(function() {
+	return $(this).find('td').map(function() {
+	  return $(this).text();
+	}).get();
+}).get();
+
+// // make chart
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
 
 
 // const ankerLiveDataContainer = document.getElementById('content').children[2];
