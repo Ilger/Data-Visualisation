e4d11296fe30d82076afc81bf89f06f0a346bba3
Ilger <ilgerdepraetere@gmail.com>
2019-07-26 12:42:11 +0200

    test: document.querySelectorAll("table"), + for of loop to log

---
+++ parseDataVisTable.js
@@ -0,0 +1,19 @@
+
+// parse table
+var tables = document.querySelectorAll("table");
+for (var table of tables) {
+    console.log(table);
+    
+  }
+// var data  = parseTable(table);
+// console.log(data);
+console.log(table);
+
+
+
+
+function parseTable(table) {
+
+    
+}
+
